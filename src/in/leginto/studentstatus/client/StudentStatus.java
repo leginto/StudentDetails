22a727e8292e99f02d8be52fd6662f71d2b718eb
leginto <leginto.r@gmail.com>
2018-07-16 00:44:19 +0530

    minor additions

--- src/in/leginto/studentstatus/client/StudentStatus.java
+++ src/in/leginto/studentstatus/client/StudentStatus.java
@@ -1,146 +1,327 @@
 package in.leginto.studentstatus.client;
 
-import in.leginto.studentstatus.shared.FieldVerifier;
+
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
-import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.event.dom.client.MouseDownEvent;
+import com.google.gwt.event.dom.client.MouseDownHandler;
+import com.google.gwt.event.dom.client.MouseOutEvent;
+import com.google.gwt.event.dom.client.MouseOutHandler;
+import com.google.gwt.event.dom.client.MouseUpEvent;
+import com.google.gwt.event.dom.client.MouseUpHandler;
+import com.google.gwt.event.logical.shared.CloseEvent;
+import com.google.gwt.event.logical.shared.CloseHandler;
+import com.google.gwt.http.client.Request;
+import com.google.gwt.http.client.RequestBuilder;
+import com.google.gwt.http.client.RequestCallback;
+import com.google.gwt.http.client.RequestException;
+import com.google.gwt.http.client.Response;
+import com.google.gwt.layout.client.Layout;
+import com.google.gwt.user.cellview.client.CellTable;
+import com.google.gwt.user.cellview.client.HasKeyboardSelectionPolicy.KeyboardSelectionPolicy;
+import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DialogBox;
-import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.MenuBar;
+import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
-/**
- * Entry point classes define <code>onModuleLoad()</code>.
- */
 public class StudentStatus implements EntryPoint {
-	/**
-	 * The message displayed to the user when the server cannot be reached or
-	 * returns an error.
-	 */
-	private static final String SERVER_ERROR = "An error occurred while "
-			+ "attempting to contact the server. Please check your network " + "connection and try again.";
+	
+	
+	private static class Students
+	{
+		private final String name;
+		private final String email;
+		private final String mobile;
+		
+		public Students(String name, String email, String mobile)
+		{
+			this.name = name;
+			this.email = email;
+			this.mobile = mobile;
+		}
+		
+		public String getName()
+		{
+			return name;
+		}
+		public String getEmail()
+		{
+			return email;
+		}
+		
+		public String getMobile()
+		{
+			return mobile;
+		}
+		
+		
+	}
+	
+	
+	
 
-	/**
-	 * Create a remote service proxy to talk to the server-side Greeting service.
-	 */
-	private final GreetingServiceAsync greetingService = GWT.create(GreetingService.class);
-
-	/**
-	 * This is the entry point method.
-	 */
 	public void onModuleLoad() {
-		final Button sendButton = new Button("Send");
-		final TextBox nameField = new TextBox();
-		nameField.setText("GWT User");
-		final Label errorLabel = new Label();
+		
+		
+		
+		RequestBuilder requestBuild = new RequestBuilder(null,"studentdata.json");
+		
+		try {
+			requestBuild.sendRequest(null, new RequestCallback() {
+				
+				@Override
+				public void onResponseReceived(Request request, Response response) {
+					// TODO Auto-generated method stub
+					
+					
+				}
+				
+				@Override
+				public void onError(Request request, Throwable exception) {
+					// TODO Auto-generated method stub
+					
+				}
+			});
+		} catch (RequestException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+		
+		CellTable<Students> table = new CellTable<Students>();
+		
+		table.setKeyboardSelectionPolicy(KeyboardSelectionPolicy.ENABLED);
+		
+		
+		
+		// create 4 columns 
+		
+		
+		
+		// read data from json file and store in CellTable
+		
+		
+		
+		
+		// dialog layout for add student
+		
+		
+		HorizontalPanel horizPan1 = new HorizontalPanel();
+		HorizontalPanel horizPan2 = new HorizontalPanel();
+		HorizontalPanel horizPan3 = new HorizontalPanel();
+		
+		
+		// text boxes
+		
+		final TextBox nameText = new TextBox();
+		final TextBox emailText = new TextBox();
+		final TextBox mobileText = new TextBox();
+		
 
-		// We can add style names to widgets
-		sendButton.addStyleName("sendButton");
-
-		// Add the nameField and sendButton to the RootPanel
-		// Use RootPanel.get() to get the entire body element
-		RootPanel.get("nameFieldContainer").add(nameField);
-		RootPanel.get("sendButtonContainer").add(sendButton);
-		RootPanel.get("errorLabelContainer").add(errorLabel);
-
-		// Focus the cursor on the name field when the app loads
-		nameField.setFocus(true);
-		nameField.selectAll();
-
-		// Create the popup dialog box
-		final DialogBox dialogBox = new DialogBox();
-		dialogBox.setText("Remote Procedure Call");
-		dialogBox.setAnimationEnabled(true);
-		final Button closeButton = new Button("Close");
-		// We can set the id of a widget by accessing its Element
-		closeButton.getElement().setId("closeButton");
-		final Label textToServerLabel = new Label();
-		final HTML serverResponseLabel = new HTML();
-		VerticalPanel dialogVPanel = new VerticalPanel();
-		dialogVPanel.addStyleName("dialogVPanel");
-		dialogVPanel.add(new HTML("<b>Sending name to the server:</b>"));
-		dialogVPanel.add(textToServerLabel);
-		dialogVPanel.add(new HTML("<br><b>Server replies:</b>"));
-		dialogVPanel.add(serverResponseLabel);
-		dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
-		dialogVPanel.add(closeButton);
-		dialogBox.setWidget(dialogVPanel);
-
-		// Add a handler to close the DialogBox
-		closeButton.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event) {
-				dialogBox.hide();
-				sendButton.setEnabled(true);
-				sendButton.setFocus(true);
+		
+		nameText.addMouseDownHandler(new MouseDownHandler() {
+			
+			@Override
+			public void onMouseDown(MouseDownEvent event) {
+				// TODO Auto-generated method stub
+				if(nameText.getText().equals("Enter Name"))
+				{
+					nameText.setText("");
+				}
+				
+			}
+		});
+		
+		nameText.addMouseOutHandler(new MouseOutHandler() {
+			
+			@Override
+			public void onMouseOut(MouseOutEvent event) {
+				// TODO Auto-generated method stub
+				if(nameText.getText().length()==0)
+				{
+					nameText.setText("Enter Name");
+				}
+				
+			}
+		});
+		//------------------
+		emailText.addMouseDownHandler(new MouseDownHandler() {
+			
+			@Override
+			public void onMouseDown(MouseDownEvent event) {
+				// TODO Auto-generated method stub
+				if(emailText.getText().equals("email@example.com"))
+				{
+					emailText.setText("");
+				}
+				
+			}
+		});
+		
+		emailText.addMouseOutHandler(new MouseOutHandler() {
+			
+			@Override
+			public void onMouseOut(MouseOutEvent event) {
+				// TODO Auto-generated method stub
+				if(emailText.getText().length()==0)
+				{
+					emailText.setText("email@example.com");
+				}
+				
+			}
+		});
+		//-----------------
+		mobileText.addMouseDownHandler(new MouseDownHandler() {
+			
+			@Override
+			public void onMouseDown(MouseDownEvent event) {
+				// TODO Auto-generated method stub
+				if(mobileText.getText().equals("Eg: 9999999999"))
+				{
+					mobileText.setText("");
+				}
+				
+			}
+		});
+		
+		mobileText.addMouseOutHandler(new MouseOutHandler() {
+			
+			@Override
+			public void onMouseOut(MouseOutEvent event) {
+				// TODO Auto-generated method stub
+				if(mobileText.getText().length()==0)
+				{
+					mobileText.setText("Eg: 9999999999");
+				}
+				
 			}
 		});
 
-		// Create a handler for the sendButton and nameField
-		class MyHandler implements ClickHandler, KeyUpHandler {
-			/**
-			 * Fired when the user clicks on the sendButton.
-			 */
+		
+		
+		
+		
+		
+		horizPan1.add(new Label(" \0 \0 Name: \0 \0  \0 \0  \0 \0  \0 \0  \0 \0 "));
+		horizPan1.add(nameText);
+		
+		horizPan2.add(new Label(" \0 \0 Email Id:\0 \0  \0 \0 \0 \0 \0"));
+		horizPan2.add(emailText);
+		
+		horizPan3.add(new Label(" \0 \0 Mobiel No: \0 \0  \0 \0 "));
+		horizPan3.add(mobileText);
+		
+		
+		// save button
+		Button saveButton = new Button("Save");
+		final VerticalPanel vertiPan = new VerticalPanel();
+		
+		vertiPan.add(horizPan1);
+		vertiPan.add(horizPan2);
+		vertiPan.add(horizPan3);
+		vertiPan.add(saveButton);
+		
+		
+		// add student command
+		
+		
+		final DialogBox addStuDialog = new DialogBox();
+		
+		
+		Command addStudent = new Command() {
+			
+			@Override
+			public void execute() {
+				// TODO Auto-generated method stub
+				//Window.alert("Student added");
+				
+				// setting values
+				
+				nameText.setText("Enter Name");
+				emailText.setText("email@example.com");
+				mobileText.setText("Eg: 9999999999");
+				
+				
+				
+				//creating a dialog box
+				
+				addStuDialog.setText("Adding a new Student");
+				addStuDialog.setGlassEnabled(true);
+				addStuDialog.center();
+				addStuDialog.add(vertiPan);
+				addStuDialog.show();
+				
+				
+				
+			}
+		};
+		
+		
+		
+		
+		
+		
+		// view student command
+		Command viewStudent = new Command() {
+			
+			@Override
+			public void execute() {
+				// TODO Auto-generated method stub
+				Window.alert("View student");
+			}
+		};
+		
+		
+		// created menu bar
+		final MenuBar mainMenu = new MenuBar();
+		
+		mainMenu.addItem("Add Student",addStudent);
+		mainMenu.addItem("View Student", viewStudent);
+		
+		
+		
+		// save button logic
+		saveButton.addClickHandler(new ClickHandler() {
+			
+			@Override
 			public void onClick(ClickEvent event) {
-				sendNameToServer();
+				// TODO Auto-generated method stub
+				addStuDialog.hide();
+				
+				// logic to add (name, email, mobile)to the existing record
+				
+				
+				
+				
+				
 			}
-
-			/**
-			 * Fired when the user types in the nameField.
-			 */
-			public void onKeyUp(KeyUpEvent event) {
-				if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-					sendNameToServer();
-				}
-			}
-
-			/**
-			 * Send the name from the nameField to the server and wait for a response.
-			 */
-			private void sendNameToServer() {
-				// First, we validate the input.
-				errorLabel.setText("");
-				String textToServer = nameField.getText();
-				if (!FieldVerifier.isValidName(textToServer)) {
-					errorLabel.setText("Please enter at least four characters");
-					return;
-				}
-
-				// Then, we send the input to the server.
-				sendButton.setEnabled(false);
-				textToServerLabel.setText(textToServer);
-				serverResponseLabel.setText("");
-				greetingService.greetServer(textToServer, new AsyncCallback<String>() {
-					public void onFailure(Throwable caught) {
-						// Show the RPC error message to the user
-						dialogBox.setText("Remote Procedure Call - Failure");
-						serverResponseLabel.addStyleName("serverResponseLabelError");
-						serverResponseLabel.setHTML(SERVER_ERROR);
-						dialogBox.center();
-						closeButton.setFocus(true);
-					}
-
-					public void onSuccess(String result) {
-						dialogBox.setText("Remote Procedure Call");
-						serverResponseLabel.removeStyleName("serverResponseLabelError");
-						serverResponseLabel.setHTML(result);
-						dialogBox.center();
-						closeButton.setFocus(true);
-					}
-				});
-			}
-		}
-
-		// Add a handler to send the name to the server
-		MyHandler handler = new MyHandler();
-		sendButton.addClickHandler(handler);
-		nameField.addKeyUpHandler(handler);
+		});
+		
+		
+		
+		RootPanel.get().add(mainMenu);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+	
 	}
 }
