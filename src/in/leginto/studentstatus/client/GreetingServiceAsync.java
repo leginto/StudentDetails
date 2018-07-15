ef485c8c1fc60abcca5f27f0f183c57fdff20dff
leginto <leginto.r@gmail.com>
2018-07-15 19:58:22 +0530

    Added source

---
+++ src/in/leginto/studentstatus/client/GreetingServiceAsync.java
@@ -0,0 +1,10 @@
+package in.leginto.studentstatus.client;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+/**
+ * The async counterpart of <code>GreetingService</code>.
+ */
+public interface GreetingServiceAsync {
+	void greetServer(String input, AsyncCallback<String> callback) throws IllegalArgumentException;
+}
